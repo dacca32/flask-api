f777899e19f99cf8d170ad41ee7168514ced15d8
Scot Darcy <scotdarcymac@gmail.com>
2024-11-22 12:04:24 +0000

    fixed docker build issue with context

--- app.py
+++ app.py
@@ -7,13 +7,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    __tablename__ = 'hitc_users'
+    __tablename__ = 'roots_users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     age = db.Column(db.Integer, nullable=False)
-  
+
+    def json(self):
+        return {'id': self.id, 'username': self.username, 'email': self.email, 'age': self.age}
+
+with app.app_context():
     db.create_all()
 
 # Headinthe clouds test route
